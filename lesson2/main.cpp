3328baa5b0c622c76843ad6895c9ef95c5fee863
Qnarik Safoyan <qnarik.safoyan@basic.local>
2021-05-01 20:35:01 +0400

    secondlesson

---
+++ lesson2/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+//int count = 122;
+//cout<<count;
+
+int age;
+int count;
+cout<<"Enter a number ";
+cin>>count;
+cout<<"Enter an age ";
+cin>>age;
+//cout<<"count = "<<count<<endl;
+//cout<<"age = "<<age;
+  return 0;
+}
